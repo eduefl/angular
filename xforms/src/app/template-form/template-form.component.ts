63699cd79a431b43d76589eda0ace2556e8578fb
Edu <ELima@national-platform.ru>
2020-04-19 19:31:42 +0300

    Updates related to class 78 from angular course https://www.youtube.com/watch?v=9lmUpszp9Vo

--- xforms/src/app/template-form/template-form.component.ts
+++ xforms/src/app/template-form/template-form.component.ts
@@ -8,8 +8,8 @@ import { Component, OnInit } from '@angular/core';
 export class TemplateFormComponent implements OnInit {
 
   usuario: any = {
-    nome: 'user',
-    email: 'user@mail.com'
+    nome: null,
+    email: null
   }
 
   onSubmit(form){
