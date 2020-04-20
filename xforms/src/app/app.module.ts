611857f8fa6c33bb871a619febf0fc4dceb8bb56
Edu <ELima@national-platform.ru>
2020-04-20 15:39:15 +0300

    Created debuig for to be able to make some 'gambiarra' to debug unoficcialy

--- xforms/src/app/app.module.ts
+++ xforms/src/app/app.module.ts
@@ -7,11 +7,12 @@ import { AppRoutingModule } from './app-routing.module';
 
 import { AppComponent } from './app.component';
 import { DataFormComponent } from './data-form/data-form.component';
+import { FormDebugComponent } from './form-debug/form-debug.component';
 
 @NgModule({
   declarations: [
     AppComponent,  
-    DataFormComponent
+    DataFormComponent, FormDebugComponent
   ],
   imports: [
     BrowserModule,
