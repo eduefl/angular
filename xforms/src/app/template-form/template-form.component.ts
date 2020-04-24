35149fa2cdc11b549298953a46c2d8c2ab753afe
Edu <ELima@national-platform.ru>
2020-04-24 20:07:04 +0300

    Changes related to class 85 https://www.youtube.com/watch?v=oLtdDwNcfoM where we learn how to consult an webservice to get CEP amd stuffs like this Important  In the original example was used the webservice from ViaCep But it is blocked in Russia so as an alternative i have usedrepublicavirtual that has access here so some adapt probably will be necessary Remembering that this block happens in Chrome only so if it  is needed we can test in opera wit viacep but also as a chalenge lets try do it with the new webservice.

--- xforms/src/app/template-form/template-form.component.ts
+++ xforms/src/app/template-form/template-form.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { Http } from '@angular/http';
+
+import 'rxjs/add/operator/map';
+
 
 @Component({
   selector: 'app-template-form',
@@ -20,7 +24,7 @@ export class TemplateFormComponent implements OnInit {
 
   }
 
-  constructor() { }
+  constructor(private http: Http) { }
 
   ngOnInit() {
   }
@@ -28,6 +32,46 @@ export class TemplateFormComponent implements OnInit {
   verificaValidTouched(campo)
   {
     return !campo.valid && campo.touched
+
+  }
+
+  consultaCep(cep){
+
+    //Nova variável "cep" somente com dígitos.
+    cep = cep.replace(/\D/g, ''); // Expressao regular que faz a subistituicao de qualquer valor nao numerico
+
+    //Verifica se campo cep possui valor informado.
+    if (cep != "") {
+      //Expressão regular para validar o CEP.
+      var validacep = /^[0-9]{8}$/;
+
+      //Valida o formato do CEP.
+      if(validacep.test(cep)) {
+        /*/Important 
+          In the original example was used the webservice from ViaCep
+          But it is blocked in Russia so as an alternative i have used
+          republicavirtual that has access here so some adapt probably will be necessary.
+
+          Remembering that this block happens in Chrome only so if it  is needed we can test in
+           opera wit viacep but also as a chalenge lets try do it with the new webservice.
+
+        /*/
+        // this.http.get("https://viacep.com.br/ws/"+ cep +"/json"); // Also possible
+        //this.http.get(`https://viacep.com.br/ws/${cep}/json`)
+        this.http.get(`http://cep.republicavirtual.com.br/web_cep.php?cep=${cep}&formato=json`)         
+        .map(dados => dados.json())
+        .subscribe(dados => console.log(dados));
+
+
+      }
+
+
+
+    }
+
+
+
+    //console.log(cep);
 
   }
 
