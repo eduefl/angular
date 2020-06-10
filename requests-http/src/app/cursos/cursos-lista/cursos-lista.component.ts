1a3f7674a168fd17dff3e74eeadba02f7992c0ea
Eduardo Ferreira Lima <elima@npl4m.ru>
2020-06-10 21:56:39 +0300

    updates related to cals 124 https://www.youtube.com/watch?v=ifyKt2a1CVk where we learned hot to cath error with pipe async and subscrible

--- requests-http/src/app/cursos/cursos-lista/cursos-lista.component.ts
+++ requests-http/src/app/cursos/cursos-lista/cursos-lista.component.ts
@@ -1,7 +1,8 @@
 import { CursosService } from './../cursos.service';
 import { Component, OnInit } from '@angular/core';
 import { Curso } from '../curso';
-import { Observable } from 'rxjs';
+import { Observable, empty, EMPTY, Subject } from 'rxjs';
+import { catchError, switchMap } from 'rxjs/operators';
 
 @Component({
   selector: 'app-cursos-lista',
@@ -11,13 +12,57 @@ import { Observable } from 'rxjs';
 export class CursosListaComponent implements OnInit {
 
  // cursos: Curso[];
-  cursos$: Observable<Curso[]>;
+ cursos$: Observable<Curso[]>;
+ error$ = new Subject <boolean>();
+
   constructor(private cursoService: CursosService) { }
 
   ngOnInit() {
    // this.cursoService.list()
    // .subscribe(resultado => this.cursos = resultado);
-   this.cursos$ = this.cursoService.list();
+   this.onRefresh();
+  }
+
+  onRefresh() {
+    this.cursos$ = this.cursoService.list()
+    .pipe(
+      catchError(error => {
+        console.error(error);
+        this.error$.next(true);
+        return EMPTY;
+      })
+    );
+  // example how to use with subscrible
+  /*/this.cursoService.list().subscribe(
+    dados => {
+      console.log(dados) ; // Sucesso
+    },
+    error => {
+       console.error(); // Erro
+    },
+     () => {
+      console.log('Observable completo');  // Observable completo
+    });/*/
+  // example how to use with subscrible and pipe
+/*/    this.cursoService.list()
+    .pipe(
+      map()
+      tap()
+      switchMap()
+      catchError(error => EMPTY)
+    )
+    .subscribe(
+    dados => {
+      console.log(dados) ; // Sucesso
+    },
+    error => {
+       console.error(); // Erro
+    },
+     () => {
+      console.log('Observable completo');  // Observable completo
+    });/*/
+
+
   }
 
 }
