afcf1f78b83a30a74faf6ac770c20930b19cd32e
Eduardo Ferreira Lima <elima@npl4m.ru>
2020-06-17 15:04:57 +0300

    fist part of the chalenge where we changes the modal amert acording to the necessities

--- requests-http/src/app/shared/alert-modal.service.ts
+++ requests-http/src/app/shared/alert-modal.service.ts
@@ -8,14 +8,25 @@ export enum ALERTTYPES  {
 
 }
 
+
 @Injectable({
   providedIn: 'root'
 })
 export class AlertModalService {
 
+  private defalutcErrorImg = 'https://cdn4.iconfinder.com/data/icons/miscellaneous-icons-2-1/200/misc_shit-512.png';
+  private defalutcSuccesImg = 'https://seeklogo.com/images/F/facebook-like-beer-icon-logo-085B2FDDC2-seeklogo.com.png';
+
   constructor(private modalService: BsModalService) { }
 
-  private Showmsg(message: string, backdrop = true, ignoreBackdropClick = true, type: ALERTTYPES, dissmisstimeOut?:number) {
+  private Showmsg(message: string,
+    backdrop = true,
+    ignoreBackdropClick = true,
+    type: ALERTTYPES,
+    dissmisstimeOut?:number,
+    img = null,
+    title = null) {
+
     const config = {
       backdrop: backdrop,
       ignoreBackdropClick: ignoreBackdropClick
@@ -23,6 +34,12 @@ export class AlertModalService {
     const bsmodalRef: BsModalRef = this.modalService.show(AlertModalComponent, config);
     bsmodalRef.content.type = type;
     bsmodalRef.content.message = message;
+    if(img){
+      bsmodalRef.content.img =  img ;
+    }
+    if(title) {
+      bsmodalRef.content.title =  title ;
+    }
 
     if(dissmisstimeOut){
       setTimeout(() => bsmodalRef.hide() , dissmisstimeOut);
@@ -31,11 +48,19 @@ export class AlertModalService {
 
   }
 
-  showAllertDanger(message: string, backdrop = true, ignoreBackdropClick = true ) {
-    this.Showmsg(message, backdrop, ignoreBackdropClick , ALERTTYPES.DANGER);
+  showAllertDanger(message: string,
+    backdrop = true,
+    ignoreBackdropClick = true ,
+    img = this.defalutcErrorImg,
+    title = 'Holy guacamole') {
+    this.Showmsg(message, backdrop, ignoreBackdropClick , ALERTTYPES.DANGER,null,img,title);
   }
-  showAllertSuccess(message: string, backdrop = true, ignoreBackdropClick = true ) {
-    this.Showmsg(message, backdrop, ignoreBackdropClick , ALERTTYPES.SUCCESS,3000);
+  showAllertSuccess(message: string,
+    backdrop = true,
+    ignoreBackdropClick = true ,
+    img = this.defalutcSuccesImg,
+    title = 'That`s science Beach') {
+    this.Showmsg(message, backdrop, ignoreBackdropClick , ALERTTYPES.SUCCESS,3000,img,title);
   }
 
 
